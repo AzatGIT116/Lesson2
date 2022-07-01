69c156c33083027ad573b83a5ea91863f9450b15
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-02 00:19:54 +0300

    add Zadacha 13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 )
+{
+  string str = number.ToString();
+  Console.WriteLine(str[2]);
+}
+else
+{ 
+  Console.WriteLine("Третьей цифры нет");
+}
